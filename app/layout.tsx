4397eaf5cbf72b716fca59b6241f8e7a6693faf5
IchaCoder <ichabordyeboah247@gmail.com>
2025-05-26 19:08:07 +0000

    added anlytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
   title: "Ronsard Voting",
@@ -17,6 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <body>{children}</body>
       <Toaster />
+      <Analytics />
     </html>
   );
 }
