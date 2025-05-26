b28d074e5e1c0057de68f7164d70a4c3ab13c9c6
IchaCoder <ichabordyeboah247@gmail.com>
2025-05-26 19:33:42 +0000

    changed time

--- components/voting-interface.tsx
+++ components/voting-interface.tsx
@@ -21,7 +21,7 @@ import { useToast } from "@/hooks/use-toast";
 // Voting schedule configuration - Update these times as needed
 const VOTING_CONFIG = {
   startTime: new Date("2025-05-26T08:00:00"), // December 20, 2024 at 8:00 AM
-  endTime: new Date("2025-05-26T24:00:00"), // December 20, 2024 at 5:00 PM
+  endTime: new Date("2025-05-27T20:00:00"), // December 20, 2024 at 5:00 PM
 };
 
 // Create validation schema dynamically based on portfolios
